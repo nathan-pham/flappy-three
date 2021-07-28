216925ec676e6bcbc01cfbbc558e20072592981b
Nathan Pham <nathanpham.me@gmail.com>
2021-07-28 13:43:04 -0700

    code cleanup

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ import Sketch from "./classes/Sketch.js"
 import DirectionalLight from "./classes/objects/lights/DirectionalLight.js"
 import AmbientLight from "./classes/objects/lights/AmbientLight.js"
 import Plane from "./classes/objects/Plane.js"
-// import Pipe from "./classes/objects/Pipe.js"
 import Cube from "./classes/objects/Cube.js"
 import Pipes from "./classes/objects/Pipes.js"
 const sketch = new Sketch({container: "#webgl__container", controls: false, debug: true})
@@ -12,14 +11,8 @@ sketch.add(
     new DirectionalLight(),
     new AmbientLight(),
     new Plane(),
-    new Cube(),
-
-    new Pipes()
-
-    // Pipe.createSet(150)
+    new Pipes(),
+    new Cube()
 )
-/*
-height + gap
-*/
 
 sketch.render()
